b836f9034b96053561d94815eb06fb2d64a3cbb3
Iyanu Taiwo <iyanutaiwo@gmail.com>
2024-02-19 22:41:40 +0000

    readmegenerator push

--- starter/index.js
+++ starter/index.js
@@ -1,21 +1,62 @@
 const fs = require("fs");
-const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-const questions = [
-
-];
-
-// function to write README file
-function writeToFile(fileName, data) {
-}
-
-// function to initialize program
-function init() {
-
-}
-
-// function call to initialize program
-init();
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a description of your project:',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the installation instructions?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How will your application be used?',
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'How can others contribute to this project?',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'What tests did you conduct on this project?',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your project:',
+        choices: ['MIT', 'GNU GPLv3', 'Apache 2.0', 'ISC', 'None'],
+    },
+    {
+        type: 'input',
+        name: 'githubUsername',
+        message: 'What is your GitHub username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+    },
+])
+.then((data) => {
+    // Generate README content
+    const readmeContent = generateMarkdown(data);
+    
+    // Write README file
+    fs.writeFile('README.md', readmeContent, (err) =>
+        err ? console.error(err) : console.log('README.md successfully generated!')
+    );
+})
--- starter/utils/generateMarkdown.js
+++ starter/utils/generateMarkdown.js
@@ -1,7 +1,37 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+# ${data.title}
 
+## Description
+${data.description}
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+This project is licensed under the ${data.license} license.
+
+## Contribution
+${data.contribution}
+
+## Tests
+${data.tests}
+
+## Questions
+If you have any questions, you can reach out to me via [GitHub](https://github.com/${data.githubUsername}) or email at ${data.email}.
 `;
 }
 
